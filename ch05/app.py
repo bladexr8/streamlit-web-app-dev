fe5e53e300bafb69347789e6c91cc9c496cfe2c6
Stephen Moss <bladexr8@gmail.com>
2025-09-05 23:36:05 +1000

    working on ch05

---
+++ ch05/app.py
@@ -0,0 +1,113 @@
+# Core Pkgs
+import streamlit as st
+st.set_page_config(page_title="NLP Web App", page_icon="👍", layout="centered", initial_sidebar_state="auto")
+
+# NLP Pkgs
+from textblob import TextBlob
+import spacy
+import neattext as nt
+
+# Viz Pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+# only use if necessae=ry
+# matplotlib.use("Agg")
+from wordcloud import WordCloud
+
+# main procedure
+def main():
+    """NLP Web App with Streamlit"""
+
+    # st.title("NLP Web App")
+    title_template = """
+        <div style="background-color:blue; padding:8px">
+            <h1 style="color:cyan">NLP Web App</h1>
+        </div>
+    """
+
+    st.markdown(title_template, unsafe_allow_html=True)
+
+    subheader_template = """
+        <div style="background-color:cyan; padding:8px">
+            <h3 style="color:blue">Powered by Streamlit</h1>
+        </div>
+    """
+
+    st.markdown(subheader_template, unsafe_allow_html=True)
+
+    st.sidebar.image("nlp.jpg", use_container_width=True)
+
+    # sidebar menu
+    activity = ["Text Analysis", "Translation", "Sentiment Analysis", "About"]
+    choice = st.sidebar.selectbox("Menu", activity)
+
+    # menu options
+    if choice == "Text Analysis":
+        st.subheader("Text Analysis")
+        st.write("")
+
+        raw_text = st.text_area("Write something", "Enter a text in English...", height=300)
+
+        if st.button("Analyze"):
+            if len(raw_text) == 0:
+                st.warning("Please Enter some Text...")
+            else:
+                blob = TextBlob(raw_text)
+                #st.write("OK")
+                st.info("Basic Functions")
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    with st.expander("Basic Info"):
+                        st.write("Text Stats")
+                        # use neattext to generate states
+                        word_desc = nt.TextFrame(raw_text).word_stats()
+                        result_desc = {
+                            "Length of Text":word_desc["Length of Text"],
+                            "Num of Vowels":word_desc["Num of Vowels"],
+                            "Num of Consonants":word_desc["Num of Consonants"],
+                            "Num of Stopwords":word_desc["Num of Stopwords"]
+                        }
+                        st.write(result_desc)
+
+                with col2:
+                    with st.expander("Processed Text"):
+                        st.success("Stopwords Excluded Text")
+
+                st.write("")
+                st.write("")
+                st.info("Advanced Features")
+
+                col3, col4 = st.columns(2)
+
+                with col3:
+                    with st.expander("Tokens&Lemmas"):
+                        st.write("T&K")
+
+                with col4:
+                    with st.expander("Summarize"):
+                        st.success("Summarize")
+
+    if choice == "Translation":
+        st.subheader("Translation")
+        st.write("")
+
+    if choice == "Sentiment Analysis":
+        st.subheader("Sentiment Analysis")
+        st.write("")
+
+    if choice == "About":
+        st.subheader("About")
+        st.write("")
+
+        st.markdown("""
+           ### NLP Web App made with Streamlit
+
+           for info:
+           - [streamlit](https://streamlit.io)         
+        """)
+
+if __name__ == "__main__":
+    main()
+
